f873fee9eddd72f11799f17a6d64896988955843
dn070017 <dn070017@gmail.com>
2021-10-20 10:51:21 +0200

    incorporate mlflow, add utils/data.py to process data

--- main.py
+++ main.py
@@ -1,191 +1,175 @@
 #%%
 import argparse
 import os
+import mlflow
 import numpy as np
 import tensorflow as tf
 import warnings
 
 from models.creator import VAE
-from utils.utils import preprocess_images, generate_and_save_images, animated_image_generation, plot_latent_images
+from utils.data import get_dataset, get_dataset_params
+from utils.utils import generate_and_save_images, animated_image_generation, plot_latent_images
 from time import time
 from IPython import display
 
+# Set GPU memory to dynamic allocation instead of using all the GPU memory
 try:
-    physical_devices = tf.config.list_physical_devices('GPU')
-    for i, d in enumerate(physical_devices):
-        tf.config.experimental.set_memory_growth(physical_devices[i], True)
+  physical_devices = tf.config.list_physical_devices('GPU')
+  for i, d in enumerate(physical_devices):
+      tf.config.experimental.set_memory_growth(physical_devices[i], True)
 except:
-    print('No GPU detected. Use CPU instead')
-
-def data_generator(X, y):
-    for image, label in zip(X, y):
-        yield {'x': image, 'y': label}
+  print('No GPU detected. Use CPU instead')
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Variational Autoencoders on MNIST Dataset.')
-    parser.add_argument(
-        '--model', type=str, default='vae',
-        help="one of the following: 'vae', 'beta-vae', 'tcvae', 'factorvae', 'rfvae', 'mlvae', 'introvae'")
-    parser.add_argument(
-        '--task', type=str, default='mnist',
-        help="one of the following: 'mnist'")
-    parser.add_argument(
-        '--beta', type=float, default=1.0,
-        help='coefficient for KL divergence used in beta-VAE (Higgins et al., 2017) or coefficient used for total correlation in beta-TCVAE (Chen, et al., 2018) and in factor-vae (γ) (Kim and Mnih 2019), default: (1.0)')
-    parser.add_argument(
-        '--num_epochs', type=int, default=25, dest='num_epochs',
-        help='maximum number of epochs used during training (default: 25)')
-    parser.add_argument(
-        '--batch_size', type=int, default=32, dest='batch_size',
-        help='batch_size used during training (default: 32)')
-    parser.add_argument(
-        '--train_size', type=int, default=6400, dest='train_size',
-        help='number of samples used during training (default: 6400)')
-    parser.add_argument(
-        '--num_generated_image', type=int, default=25, dest='test_size',
-        help='number of generated images (default: 16)')
-    parser.add_argument(
-        '--latent_dim', type=int, default=2, dest='latent_dim',
-        help='number of latent embeddings (default: 2)')
-    parser.add_argument(
-        '--prefix', type=str, default='vae', dest='prefix',
-        help='directory (default: vae)')
-    parser.add_argument(
-        '--outdir', type=str, default='./tmp/', dest='outdir',
-        help='directory (default: ./tmp/)')
+  parser = argparse.ArgumentParser(description='Variational Autoencoders on MNIST Dataset.')
+  parser.add_argument(
+    '--model', type=str, default='vae',
+    help="one of the following: 'vae', 'beta-vae', 'tcvae', 'factorvae', 'rfvae', 'mlvae', 'introvae'")
+  parser.add_argument(
+    '--task', type=str, default='mnist',
+    help="one of the following: 'mnist'")
+  parser.add_argument(
+    '--beta', type=float, default=1.0,
+    help='coefficient for KL divergence used in beta-VAE (Higgins et al., 2017) or coefficient used for total correlation in beta-TCVAE (Chen, et al., 2018) and in factor-vae (γ) (Kim and Mnih 2019), default: (1.0)')
+  parser.add_argument(
+    '--num_epochs', type=int, default=25, dest='num_epochs',
+    help='maximum number of epochs used during training (default: 25)')
+  parser.add_argument(
+    '--batch_size', type=int, default=32, dest='batch_size',
+    help='batch_size used during training (default: 32)')
+  parser.add_argument(
+    '--train_size', type=int, default=6400, dest='train_size',
+    help='number of samples used during training (default: 6400)')
+  parser.add_argument(
+    '--num_generated_image', type=int, default=25, dest='test_size',
+    help='number of generated images (default: 16)')
+  parser.add_argument(
+    '--latent_dim', type=int, default=2, dest='latent_dim',
+    help='number of latent embeddings (default: 2)')
+  parser.add_argument(
+    '--prefix', type=str, default='vae', dest='prefix',
+    help='directory (default: vae)')
+  parser.add_argument(
+    '--outdir', type=str, default='./tmp/', dest='outdir',
+    help='directory (default: ./tmp/)')
 
-    args = parser.parse_args()
-    return args
+  args = parser.parse_args()
+  args.test_size = int(np.sqrt(args.test_size)) ** 2
+
+  if args.model.lower() in ['mlvae', 'multi-level-vae']:
+    args.latent_dim = max(2, args.latent_dim - 2)
+
+  return args
 
 def main(model_name, task, beta, num_epochs, train_size, batch_size, latent_dim, test_size, outdir, prefix, show_images):
-    test_size = int(np.sqrt(test_size)) ** 2
+  experiment_name = "VariationalAutoencoder"
+  mlflow.set_experiment(experiment_name)
+  experiment = mlflow.get_experiment_by_name(experiment_name)
 
-    if task.lower() == 'mnist':
-        (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-        input_dims = (28, 28, 1)
-        kernel_size = (3, 3)
-        strides = (2, 2)
+  os.makedirs(outdir, exist_ok=True)
+  os.makedirs(os.path.join(outdir, 'logging'), exist_ok=True)
 
-    os.makedirs(outdir, exist_ok=True)
+  optimizers = {
+    'primary': tf.keras.optimizers.Adam(5e-3),
+    'secondary': tf.keras.optimizers.Adam(5e-3)
+  }
 
-    train_labels = tf.one_hot(train_labels, 10)
-    test_labels = tf.one_hot(test_labels, 10)
-    train_images = preprocess_images(train_images)
-    test_images = preprocess_images(test_images)
+  train_dataset, test_dataset = get_dataset(task, train_size, test_size, batch_size)
+  input_dims, kernel_size, strides = get_dataset_params(task)
 
-    train_dataset = tf.data.Dataset.from_generator(
-        data_generator,
-        output_types={'x': tf.float32, 'y': tf.float32},
-        args=(train_images, train_labels)
-    ).shuffle(train_size).take(train_size).batch(batch_size)
-
-    test_dataset = tf.data.Dataset.from_generator(
-        data_generator,
-        output_types={'x': tf.float32, 'y': tf.float32},
-        args=(test_images, test_labels)
-    ).shuffle(test_size).take(test_size).batch(test_size)
-
-    optimizers = {
-        'primary': tf.keras.optimizers.Adam(5e-4),
-        'secondary': tf.keras.optimizers.Adam(1e-4)
+  model = VAE.create_model(
+    model_name, 
+    {
+      'latent_dim': latent_dim, 
+      'prefix': prefix,
+      'input_dims': input_dims,
+      'kernel_size': kernel_size,
+      'strides': strides
     }
-
-    if model_name.lower() in ['mlvae', 'multi-level-vae']:
-        latent_dim = max(2, latent_dim - 2)
-
-    model = VAE.create_model(
-        model_name, 
-        {
-            'latent_dim': latent_dim, 
-            'prefix': prefix,
-            'input_dims': input_dims,
-            'kernel_size': kernel_size,
-            'strides': strides
-        }
-    )
-
+  )
+  
+  with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
+    print('tracking uri:', mlflow.get_tracking_uri())
+    print('artifact uri:', mlflow.get_artifact_uri())
+    mlflow.log_params({
+      'latent_dim': latent_dim, 
+      'prefix': prefix,
+      'input_dims': input_dims,
+      'kernel_size': kernel_size,
+      'strides': strides
+    })
     for epoch in range(1, num_epochs + 1):
-        start_time = time()
-        for train_x in train_dataset:
-            elbo, logpx_z, kl_divergence = model.train_step(
-                train_x, optimizers, beta=beta, epoch=epoch)
-
-        end_time = time()
-        display.clear_output(wait=False)
-        for test_x in test_dataset:
-            elbo, logpx_z, kl_divergence = model.elbo(test_x, beta=beta)
-        
-        if epoch % 5 == 1:
-            plot_latent_images(model, train_x, outdir, 10, show_images=show_images)
-
-        message = ''.join(
-            f'Epoch: {epoch:>5}\tTest ELBO: {elbo:>.2f}\t'
-            f'Test Reconstructed Loss: {logpx_z:>.5f}\tTest KL-Divergence: '
-            f'{kl_divergence:>.2f}\tTime Elapse: {end_time - start_time:.3f}'
+      start_time = time()
+      for train_x in train_dataset:
+        elbo, logpx_z, kl_divergence = model.train_step(
+          train_x, optimizers, beta=beta, epoch=epoch
         )
-        print(message)
-        generate_and_save_images(model, outdir, epoch, test_x, show_images)
-        
+              
+      mlflow.log_metrics({
+        'train_elbo': elbo.numpy(),
+        'train_logpx_z': logpx_z.numpy(),
+        'train_kl_divergence': kl_divergence.numpy()
+      }, step=epoch)
+
+      end_time = time()
+      display.clear_output(wait=False)
+      for test_x in test_dataset:
+        elbo, logpx_z, kl_divergence = model.elbo(test_x, beta=beta)
+
+      mlflow.log_metrics({
+        'test_elbo': elbo.numpy(),
+        'test_logpx_z': logpx_z.numpy(),
+        'test_kl_divergence': kl_divergence.numpy()
+      }, step=epoch)
+      
+      message = ''.join(
+        f'Epoch: {epoch:>5}\tTest ELBO: {elbo:>.2f}\t'
+        f'Test Reconstructed Loss: {logpx_z:>.5f}\tTest KL-Divergence: '
+        f'{kl_divergence:>.2f}\tTime Elapse: {end_time - start_time:.3f}'
+      )
+      print(message)
+      generate_and_save_images(model, outdir, epoch, test_x, show_images)
+      plot_latent_images(model, train_x, outdir, 10, epoch, show_images=show_images)
+
+      if epoch % 5 == 1:
+        model.save_weights(os.path.join(outdir, f'{model.prefix}-{run.info.run_id}-epoch-{epoch}.ckpt'))
+
     animated_image_generation(model, outdir)
-    plot_latent_images(model, train_x, outdir, 10, show_images=show_images)
+    model.save_weights(os.path.join(outdir, f'{model.prefix}-{run.info.run_id}.ckpt'))
 
     return model
 
 #%%
 if __name__ == "__main__":
-    try:
-        args = parse_arguments()
-        # from command line/ debugger (in VSCODE)
-        model = main(
-            args.model,
-            args.task,
-            args.beta,
-            args.num_epochs,
-            args.train_size,
-            args.batch_size,
-            args.latent_dim,
-            args.test_size,
-            args.outdir,
-            args.prefix,
-            show_images=False
-        )
-    except:
-        # from iPython (in VSCODE)
-        warnings.warn('there is an error in the argument, use default parameters instead')
-        model = main(
-            model_name='mfcvae',
-            task='mnist',
-            beta=1.0,
-            num_epochs=50,
-            train_size=60000, # 12800
-            batch_size=128, # 64
-            latent_dim=2,
-            test_size=25,
-            outdir='tmp',
-            prefix='mfcvae', 
-            show_images=True
-        )
-
-
-# %%
-def generate(model, z=None, facet=0, num_generated_images=15, **kwargs):
-
-    if z is None:
-        z = tf.random.normal(shape=(num_generated_images, model.latent_dim), dtype=tf.float32)
-
-    if 'target' in kwargs:
-        target = kwargs['target']
-        z = tf.random.normal(
-            shape=(num_generated_images, model.latent_dim),
-            mean=model.params.mean_z_y[facet][:, target],
-            stddev=tf.sqrt(tf.exp(0.5 * model.params.logvar_z_y[facet][:, target])),
-            dtype=tf.float32
-        )
-
-    for _ in range(facet, -1, -1):
-        mean = model.mean_linear_layers[_](z)
-        var = model.var_softplus_layers[_](z)
-        z = model.reparameterize(mean, var)
-
-    return model.decode(z, apply_sigmoid=True)
-
+  try:
+    args = parse_arguments()
+    # from command line/ debugger (in VSCODE)
+    model = main(
+      args.model,
+      args.task,
+      args.beta,
+      args.num_epochs,
+      args.train_size,
+      args.batch_size,
+      args.latent_dim,
+      args.test_size,
+      args.outdir,
+      args.prefix,
+      show_images=False
+    )
+  except:
+    # from iPython (in VSCODE)
+    warnings.warn('there is an error in the argument, use default parameters instead')
+    model = main(
+      model_name='mfcvae',
+      task='mnist',
+      beta=1.0,
+      num_epochs=50,
+      train_size=60000, # 12800 / 60000
+      batch_size=512,   # 64 / 128
+      latent_dim=2,
+      test_size=25,
+      outdir='tmp',
+      prefix='mfcvae', 
+      show_images=True
+    )
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import glob
 import imageio
 import matplotlib.pyplot as plt
+import mlflow
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -8,15 +9,9 @@ import os
 
 import PIL
 
-def preprocess_images(images, dims=(-1, 28, 28, 1)):
-    images = images.reshape(dims) / 255.
-    return np.where(images > .5, 1.0, 0.0).astype('float32')
-
-
 def compute_output_dims(input_dims, kernel_size, strides):
     output_shape = np.round((input_dims - kernel_size) / strides + 0.5) + 1.
     return np.array(output_shape, dtype=np.int32)
-
 
 def generate_and_save_images(model, path, epoch, test_x, show_images=False):
   mean, logvar, z, x_pred = model.forward(test_x, apply_sigmoid=True)
@@ -31,12 +26,14 @@ def generate_and_save_images(model, path, epoch, test_x, show_images=False):
     plt.imshow(x_pred[i, :, :, 0], cmap='gray')
     plt.axis('off')
 
-  plt.savefig(os.path.join(path, f'{model.prefix}_epoch_{epoch:04d}.png'))
+  filename = os.path.join(path, f'{model.prefix}_epoch_{epoch:04d}.png')
+  plt.savefig(filename)
   if show_images:
     plt.show()
   else:
     plt.close()
   
+  mlflow.log_artifact(filename)
 
 def animated_image_generation(model, path):
   anim_file = os.path.join(path, f'{model.prefix}.gif')
@@ -49,9 +46,11 @@ def animated_image_generation(model, path):
         writer.append_data(image)
     image = imageio.imread(filename)
     writer.append_data(image)
+  
+  mlflow.log_artifact(anim_file)
 
 
-def plot_latent_images(model, batch, path, n, digit_size=28, show_images=False):
+def plot_latent_images(model, batch, path, n, epoch, digit_size=28, show_images=False):
   norm = tfp.distributions.Normal(0, 1)
   grid_x = norm.quantile(np.linspace(0.05, 0.95, n))
   grid_y = norm.quantile(np.linspace(0.05, 0.95, n))
@@ -69,7 +68,8 @@ def plot_latent_images(model, batch, path, n, digit_size=28, show_images=False):
   if type(model).__name__ == 'MLVAE':
     latent_dim += model.latent_content_dim
     importance_order = tf.argsort(
-      kl_divergence[model.latent_content_dim:], axis=-1, direction='DESCENDING', stable=False, name=None
+      kl_divergence[model.latent_content_dim:],
+      axis=-1, direction='DESCENDING', stable=False, name=None
     )
     importance_order += model.latent_content_dim
 
@@ -85,15 +85,17 @@ def plot_latent_images(model, batch, path, n, digit_size=28, show_images=False):
       z = tf.convert_to_tensor(z, dtype=tf.float32)
       x_decoded = model.generate(z, target=target)
       digit = tf.reshape(x_decoded[0], (digit_size, digit_size))
-      image[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = digit.numpy()
-    target += 1
+      image[i * digit_size: (i + 1) * digit_size, j * digit_size: (j + 1) * digit_size] = digit.numpy()
+    target += 1  
 
   plt.figure(figsize=(10, 10))
   plt.imshow(image, cmap='Greys_r')
   plt.axis('Off')
-  plt.savefig(os.path.join(path, f'{model.prefix}_latent_embedding.png'))
+
+  filename = os.path.join(path, f'{model.prefix}_latent_embedding_epoch_{epoch:04d}.png')
+  plt.savefig(filename)
   if show_images:
     plt.show()
   else:
     plt.close()
+  mlflow.log_artifact(filename)
